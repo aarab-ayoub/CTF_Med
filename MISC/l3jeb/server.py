7ccf35fd1e58f9c0b70f6cb5a9bbb16278768be6
ayoub <ayoubaarab571@gmail.com>
2025-05-04 22:03:37 +0100

    eh

--- MISC/l3jeb/server.py
+++ MISC/l3jeb/server.py
@@ -75,7 +75,7 @@ def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    host = '127.0.0.1'  # Local testing
+    host = '127.0.0.1'
     port = 9999
     
     server.bind((host, port))
